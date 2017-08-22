dab722dcdcb00e4c6ce565255dde32920e26fe02
Florent Kermarrec <florent@enjoy-digital.fr>
2017-08-22 12:18:12 +0200

    litesdcard/convert: cleanup

--- litesdcard/convert.py
+++ litesdcard/convert.py
@@ -5,8 +5,8 @@ from litex.soc.interconnect import stream
 
 class Stream32to8(Module):
     def __init__(self):
-        self.sink = stream.Endpoint([("data", 32), ("cnt", 2)])
-        self.source = stream.Endpoint([("data",8)])
+        self.sink = sink = stream.Endpoint([("data", 32), ("cnt", 2)])
+        self.source = source = stream.Endpoint([("data", 8)])
 
         mux = Signal(2)
         cnt = Signal(2)
@@ -16,23 +16,23 @@ class Stream32to8(Module):
         busy = Signal()
         cases = {}
         for i in range(3):
-            cases[i+1] = self.source.data.eq(data[(i+1)*8:(i+2)*8])
-        cases[0] = self.source.data.eq(self.sink.data[0:8])
+            cases[i+1] = source.data.eq(data[(i+1)*8:(i+2)*8])
+        cases[0] = source.data.eq(sink.data[0:8])
 
         self.sync += [
-            If(~busy & self.sink.valid & (self.sink.cnt > 0),
-                data.eq(self.sink.data),
+            If(~busy & sink.valid & (sink.cnt > 0),
+                data.eq(sink.data),
                 busy.eq(1),
-                last.eq(self.sink.last)
-            ).Elif((mux == self.sink.cnt) & self.sink.ready,
+                last.eq(sink.last)
+            ).Elif((mux == sink.cnt) & sink.ready,
                 busy.eq(0),
             ),
 
-            If(self.sink.valid & (self.sink.cnt > 0) &  (cnt != self.sink.cnt) & self.source.ready,
+            If(sink.valid & (sink.cnt > 0) &  (cnt != sink.cnt) & source.ready,
                 cnt.eq(cnt + 1)
             ),
 
-            If((cnt == self.sink.cnt) & self.source.ready,
+            If((cnt == sink.cnt) & source.ready,
                cnt.eq(0)
             )
         ]
@@ -40,17 +40,17 @@ class Stream32to8(Module):
         self.comb += [
             Case(mux, cases),
 
-            If(self.sink.valid & (self.sink.cnt==0),
+            If(sink.valid & (sink.cnt == 0),
                 mux.eq(0),
-                self.source.valid.eq(1),
-                self.source.last.eq(self.sink.last),
-                self.sink.ready.eq(self.source.ready)
-            ).Elif(self.sink.valid & (self.sink.cnt > 0) &  (cnt <= self.sink.cnt),
+                source.valid.eq(1),
+                source.last.eq(sink.last),
+                sink.ready.eq(source.ready)
+            ).Elif(sink.valid & (sink.cnt > 0) &  (cnt <= sink.cnt),
                 mux.eq(cnt),
-                self.source.valid.eq(1),
-                If(cnt == self.sink.cnt,
-                    self.source.last.eq(last),
-                    self.sink.ready.eq(self.source.ready),
+                source.valid.eq(1),
+                If(cnt == sink.cnt,
+                    source.last.eq(last),
+                    sink.ready.eq(source.ready),
                 )
             )
         ]
@@ -58,9 +58,13 @@ class Stream32to8(Module):
 
 class Stream8to32(Module):
     def __init__(self):
-        self.sink = stream.Endpoint([("data",8)])
-        self.submodules.fifo = stream.SyncFIFO([("data", 32), ('cnt', 2)], 4)
-        self.source = self.fifo.source
+        self.sink = sink = stream.Endpoint([("data", 8)])
+        self.source = source = stream.Endpoint([("data", 32), ('cnt', 2)])
+
+        # # #
+
+        fifo = stream.SyncFIFO(source.description, 4)
+        self.submodules += fifo
 
         data = Signal(32)
         mux = Signal(2)
@@ -69,27 +73,29 @@ class Stream8to32(Module):
         sink_cases = {}
 
         for i in range(4):
-            cases[i] = data[(i)*8:(i+1)*8].eq(self.sink.data)
+            cases[i] = data[(i)*8:(i+1)*8].eq(sink.data)
 
-        sink_cases[0] = self.fifo.sink.data.eq(self.sink.data)
+        sink_cases[0] = fifo.sink.data.eq(sink.data)
         for i in range(3):
-            sink_cases[i+1] = self.fifo.sink.data.eq(Cat(data[0:8*(i+1)], self.sink.data))
+            sink_cases[i+1] = fifo.sink.data.eq(Cat(data[0:8*(i+1)], sink.data))
 
         self.comb += [
             Case(mux, sink_cases),
-            If(self.sink.valid & ((mux == 3) | self.sink.last),
-                self.fifo.sink.valid.eq(1)
+            If(sink.valid & ((mux == 3) | sink.last),
+                fifo.sink.valid.eq(1)
             ),
-            self.fifo.sink.cnt.eq(mux),
-            self.fifo.sink.last.eq(self.sink.last),
-            self.sink.ready.eq(self.fifo.sink.ready),
+            fifo.sink.cnt.eq(mux),
+            fifo.sink.last.eq(sink.last),
+            sink.ready.eq(fifo.sink.ready),
+
+            fifo.source.connect(source)
         ]
 
         self.sync += [
-            If(self.sink.valid,
+            If(sink.valid,
                 Case(mux, cases),
-                If(self.fifo.sink.ready,
-                    If(self.sink.last,
+                If(fifo.sink.ready,
+                    If(sink.last,
                         mux.eq(0)
                     ).Else(
                         mux.eq(mux + 1)
