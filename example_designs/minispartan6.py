29042aaff3ea45ac0bb52c0a622358d9f2a3564f
Florent Kermarrec <florent@enjoy-digital.fr>
2017-10-10 11:55:28 +0200

    example_design: add minispartan6 (untested), simplify analyzer

--- example_designs/minispartan6.py
+++ example_designs/minispartan6.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
 import sys
+from fractions import Fraction
 
 from litex.gen import *
 from litex.gen.genlib.resetsync import AsyncResetSynchronizer
 
 from litex.build.generic_platform import *
-from litex.build.xilinx import VivadoProgrammer
+from litex.build.xilinx import FpgaProg
 
 from litex.soc.interconnect import stream
 from litex.soc.cores.uart import UARTWishboneBridge
@@ -18,55 +19,68 @@ from litex.soc.integration.builder import *
 from litex.soc.interconnect import wishbone
 
 from litesdcard.phy import SDPHY
-from litesdcard.clocker import SDClockerS7
+from litesdcard.clocker import SDClockerS6
 from litesdcard.core import SDCore
 from litesdcard.bist import BISTBlockGenerator, BISTBlockChecker
 
 from litesdcard.emulator import SDEmulator, _sdemulator_pads
 
-from litex.boards.platforms import arty
+from litex.boards.platforms import minispartan6
 
 from litescope import LiteScopeAnalyzer
 
 
 _sd_io = [
     ("sdcard", 0,
-        Subsignal("data", Pins("V11 T13 U13 U12"), Misc("PULLUP")),
-        Subsignal("cmd", Pins("V10"), Misc("PULLUP")),
-        Subsignal("clk", Pins("V12")),
+        Subsignal("data", Pins("M10 L10 J11 K12"), Misc("PULLUP")),
+        Subsignal("cmd", Pins("K11"), Misc("PULLUP")),
+        Subsignal("clk", Pins("L12")),
         IOStandard("LVCMOS33"), Misc("SLEW=FAST")
     )
 ]
 
 
 class _CRG(Module):
-    def __init__(self, platform):
+    def __init__(self, platform, clk_freq):
         self.clock_domains.cd_sys = ClockDomain()
 
-        # # #
-
-        clk100 = platform.request("clk100")
-        rst = ~platform.request("cpu_reset")
-
-        pll_locked = Signal()
+        f0 = 32*1000000
+        clk32 = platform.request("clk32")
+        clk32a = Signal()
+        self.specials += Instance("IBUFG", i_I=clk32, o_O=clk32a)
+        clk32b = Signal()
+        self.specials += Instance("BUFIO2", p_DIVIDE=1,
+                                  p_DIVIDE_BYPASS="TRUE", p_I_INVERT="FALSE",
+                                  i_I=clk32a, o_DIVCLK=clk32b)
+        f = Fraction(int(clk_freq), int(f0))
+        n, m, p = f.denominator, f.numerator, 16
+        assert f0/n*m == clk_freq
+        pll_lckd = Signal()
         pll_fb = Signal()
-        pll_sys = Signal()
-        self.specials += [
-            Instance("PLLE2_BASE",
-                     p_STARTUP_WAIT="FALSE", o_LOCKED=pll_locked,
-
-                     # VCO @ 1600 MHz
-                     p_REF_JITTER1=0.01, p_CLKIN1_PERIOD=10.0,
-                     p_CLKFBOUT_MULT=16, p_DIVCLK_DIVIDE=1,
-                     i_CLKIN1=clk100, i_CLKFBIN=pll_fb, o_CLKFBOUT=pll_fb,
-
-                     # 100 MHz
-                     p_CLKOUT0_DIVIDE=16, p_CLKOUT0_PHASE=0.0,
-                     o_CLKOUT0=pll_sys
-            ),
-            Instance("BUFG", i_I=pll_sys, o_O=self.cd_sys.clk),
-            AsyncResetSynchronizer(self.cd_sys, ~pll_locked | rst),
-        ]
+        pll = Signal(6)
+        self.specials.pll = Instance("PLL_ADV", p_SIM_DEVICE="SPARTAN6",
+                                     p_BANDWIDTH="OPTIMIZED", p_COMPENSATION="INTERNAL",
+                                     p_REF_JITTER=.01, p_CLK_FEEDBACK="CLKFBOUT",
+                                     i_DADDR=0, i_DCLK=0, i_DEN=0, i_DI=0, i_DWE=0, i_RST=0, i_REL=0,
+                                     p_DIVCLK_DIVIDE=1, p_CLKFBOUT_MULT=m*p//n, p_CLKFBOUT_PHASE=0.,
+                                     i_CLKIN1=clk32b, i_CLKIN2=0, i_CLKINSEL=1,
+                                     p_CLKIN1_PERIOD=1000000000/f0, p_CLKIN2_PERIOD=0.,
+                                     i_CLKFBIN=pll_fb, o_CLKFBOUT=pll_fb, o_LOCKED=pll_lckd,
+                                     o_CLKOUT0=pll[0], p_CLKOUT0_DUTY_CYCLE=.5,
+                                     o_CLKOUT1=pll[1], p_CLKOUT1_DUTY_CYCLE=.5,
+                                     o_CLKOUT2=pll[2], p_CLKOUT2_DUTY_CYCLE=.5,
+                                     o_CLKOUT3=pll[3], p_CLKOUT3_DUTY_CYCLE=.5,
+                                     o_CLKOUT4=pll[4], p_CLKOUT4_DUTY_CYCLE=.5,
+                                     o_CLKOUT5=pll[5], p_CLKOUT5_DUTY_CYCLE=.5,
+                                     p_CLKOUT0_PHASE=0., p_CLKOUT0_DIVIDE=p//1, # sys
+                                     p_CLKOUT1_PHASE=0., p_CLKOUT1_DIVIDE=p//1,
+                                     p_CLKOUT2_PHASE=0., p_CLKOUT2_DIVIDE=p//1,
+                                     p_CLKOUT3_PHASE=0., p_CLKOUT3_DIVIDE=p//1,
+                                     p_CLKOUT4_PHASE=0., p_CLKOUT4_DIVIDE=p//1,
+                                     p_CLKOUT5_PHASE=0., p_CLKOUT5_DIVIDE=p//1,
+        )
+        self.specials += Instance("BUFG", i_I=pll[0], o_O=self.cd_sys.clk)
+        self.specials += AsyncResetSynchronizer(self.cd_sys, ~pll_lckd)
 
 
 class SDSoC(SoCCore):
@@ -83,9 +97,9 @@ class SDSoC(SoCCore):
     csr_map.update(SoCCore.csr_map)
 
     def __init__(self, with_cpu, with_emulator, with_analyzer):
-        platform = arty.Platform()
+        platform = minispartan6.Platform(device="xc6slx25")
         platform.add_extension(_sd_io)
-        clk_freq = int(100e6)
+        clk_freq = int(50e6)
         sd_freq = int(100e6)
         SoCCore.__init__(self, platform,
                          clk_freq=clk_freq,
@@ -99,7 +113,7 @@ class SDSoC(SoCCore):
                          integrated_sram_size=0x1000,
                          integrated_main_ram_size=0x8000 if with_cpu else 0)
 
-        self.submodules.crg = _CRG(platform)
+        self.submodules.crg = _CRG(platform, clk_freq)
 
         if not with_cpu:
             self.add_cpu_or_bridge(UARTWishboneBridge(platform.request("serial"), clk_freq, baudrate=115200))
@@ -111,7 +125,7 @@ class SDSoC(SoCCore):
         else:
             sdcard_pads = platform.request('sdcard')
 
-        self.submodules.sdclk = SDClockerS7()
+        self.submodules.sdclk = SDClockerS6()
         self.submodules.sdphy = SDPHY(sdcard_pads, platform.device)
         self.submodules.sdcore = SDCore(self.sdphy)
         self.submodules.sdtimer = Timer()
@@ -126,26 +140,16 @@ class SDSoC(SoCCore):
 
         self.platform.add_period_constraint(self.crg.cd_sys.clk, 1e9/clk_freq)
         self.platform.add_period_constraint(self.sdclk.cd_sd.clk, 1e9/sd_freq)
-        self.platform.add_period_constraint(self.sdclk.cd_sd_fb.clk, 1e9/sd_freq)
 
         self.crg.cd_sys.clk.attr.add("keep")
         self.sdclk.cd_sd.clk.attr.add("keep")
-        self.sdclk.cd_sd_fb.clk.attr.add("keep")
         self.platform.add_false_path_constraints(
             self.crg.cd_sys.clk,
-            self.sdclk.cd_sd.clk,
-            self.sdclk.cd_sd_fb.clk)
+            self.sdclk.cd_sd.clk)
 
         led_counter = Signal(32)
         self.sync.sd += led_counter.eq(led_counter + 1)
         self.comb += platform.request("user_led", 0).eq(led_counter[26])
-
-        self.specials += Instance("ODDR2", p_DDR_ALIGNMENT="NONE",
-            p_INIT=1, p_SRTYPE="SYNC",
-            i_D0=0, i_D1=1, i_S=0, i_R=0, i_CE=1,
-            i_C0=ClockSignal("sd"), i_C1=~ClockSignal("sd"),
-            o_Q=platform.request("pmoda")[0]
-        )
 
         # analyzer
         if with_analyzer:
@@ -182,7 +186,7 @@ def main():
         soc.do_exit(vns)
     elif load:
         print("[loading]...")
-        prog = VivadoProgrammer()
+        prog = FpgaProg()
         prog.load_bitstream("build/gateware/top.bit")
 
 
