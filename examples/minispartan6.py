e58dd433f40cfffdfb3fc4b5a18c2bcec48494d8
Florent Kermarrec <florent@enjoy-digital.fr>
2020-01-09 12:16:39 +0100

    examples: improve readability/presentation, only constraint input clock and sd clock

--- examples/minispartan6.py
+++ examples/minispartan6.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 
-# This file is Copyright (c) 2017-2019 Florent Kermarrec <florent@enjoy-digital.fr>
+# This file is Copyright (c) 2017-2020 Florent Kermarrec <florent@enjoy-digital.fr>
 # License: BSD
 
 import sys
-from fractions import Fraction
 
 from migen import *
 from migen.genlib.resetsync import AsyncResetSynchronizer
 
+from litex.boards.platforms import minispartan6
+
 from litex.build.generic_platform import *
-from litex.build.openocd import OpenOCD
 
 from litex.soc.interconnect import stream
 from litex.soc.cores.uart import UARTWishboneBridge
@@ -20,19 +20,13 @@ from litex.soc.cores.clock import *
 from litex.soc.interconnect.csr import *
 from litex.soc.integration.soc_core import *
 from litex.soc.integration.builder import *
-from litex.soc.interconnect import wishbone
 
 from litesdcard.phy import SDPHY
 from litesdcard.clocker import SDClockerS6
 from litesdcard.core import SDCore
 from litesdcard.bist import BISTBlockGenerator, BISTBlockChecker
 
-from litesdcard.emulator import SDEmulator, _sdemulator_pads
-
-from litex.boards.platforms import minispartan6
-
-from litescope import LiteScopeAnalyzer
-
+# CRG ----------------------------------------------------------------------------------------------
 
 class _CRG(Module):
     def __init__(self, platform, sys_clk_freq):
@@ -40,46 +34,55 @@ class _CRG(Module):
 
         # # #
 
+        clk32 = platform.request("clk32")
+        platform.add_period_constraint(clk32, 1e9/32e6)
+
         self.submodules.pll = pll = S6PLL(speedgrade=-1)
-        pll.register_clkin(platform.request("clk32"), 32e6)
+        pll.register_clkin(clk32, 32e6)
         pll.create_clkout(self.cd_sys, sys_clk_freq)
 
+# SDCardSoC ----------------------------------------------------------------------------------------
 
 class SDSoC(SoCCore):
     def __init__(self, with_cpu, with_emulator, with_analyzer):
         platform = minispartan6.Platform(device="xc6slx25")
-        clk_freq = int(50e6)
-        sd_freq = int(140e6)
+        sys_clk_freq = int(50e6)
+        sd_clk_freq  = int(100e6)
+
+        # SoCCore ----------------------------------------------------------------------------------
         SoCCore.__init__(self, platform,
-                         clk_freq=clk_freq,
-                         cpu_type="lm32" if with_cpu else None,
-                         csr_data_width=32,
-                         with_uart=with_cpu,
-                         with_timer=with_cpu,
-                         ident="SDCard Test SoC",
-                         ident_version=True,
-                         integrated_rom_size=0x8000 if with_cpu else 0,
-                         integrated_main_ram_size=0x8000 if with_cpu else 0)
+            clk_freq                 = sys_clk_freq,
+            cpu_type                 = "lm32" if with_cpu else None,
+            csr_data_width           = 32,
+            with_uart                = with_cpu,
+            with_timer               = with_cpu,
+            ident                    = "SDCard Test SoC",
+            ident_version            = True,
+            integrated_rom_size      = 0x8000 if with_cpu else 0,
+            integrated_main_ram_size = 0x8000 if with_cpu else 0
+        )
 
-        self.submodules.crg = _CRG(platform, clk_freq)
+        # CRG --------------------------------------------------------------------------------------
+        self.submodules.crg = _CRG(platform, sys_clk_freq)
 
-        # bridge
+        # Serial bridge (optional) -----------------------------------------------------------------
         if not with_cpu:
-            self.submodules.bridge = UARTWishboneBridge(platform.request("serial"), clk_freq, baudrate=115200)
+            self.submodules.bridge = UARTWishboneBridge(platform.request("serial"), sys_clk_freq)
             self.add_wb_master(self.bridge.wishbone)
 
-        # emulator
+        # SDCard Emulator (optional) ---------------------------------------------------------------
         if with_emulator:
+            from litesdcard.emulator import SDEmulator, _sdemulator_pads
             sdcard_pads = _sdemulator_pads()
             self.submodules.sdemulator = SDEmulator(platform, sdcard_pads)
             self.add_csr("sdemulator")
         else:
             sdcard_pads = platform.request('sdcard')
 
-        # sd
-        self.submodules.sdclk = SDClockerS6()
-        self.submodules.sdphy = SDPHY(sdcard_pads, platform.device)
-        self.submodules.sdcore = SDCore(self.sdphy)
+        # SDCard -----------------------------------------------------------------------------------
+        self.submodules.sdclk   = SDClockerS6()
+        self.submodules.sdphy   = SDPHY(sdcard_pads, platform.device)
+        self.submodules.sdcore  = SDCore(self.sdphy)
         self.submodules.sdtimer = Timer()
         self.add_csr("sdclk")
         self.add_csr("sdphy")
@@ -87,29 +90,24 @@ class SDSoC(SoCCore):
         self.add_csr("sdtimer")
 
         self.submodules.bist_generator = BISTBlockGenerator(random=True)
-        self.submodules.bist_checker = BISTBlockChecker(random=True)
+        self.submodules.bist_checker   = BISTBlockChecker(random=True)
         self.add_csr("bist_generator")
         self.add_csr("bist_checker")
-
         self.comb += [
             self.sdcore.source.connect(self.bist_checker.sink),
             self.bist_generator.source.connect(self.sdcore.sink)
         ]
+        self.platform.add_period_constraint(self.sdclk.cd_sd.clk, 1e9/sd_clk_freq)
+        self.platform.add_false_path_constraints(self.crg.cd_sys.clk, self.sdclk.cd_sd.clk)
 
-        self.platform.add_period_constraint(self.crg.cd_sys.clk, 1e9/clk_freq)
-        self.platform.add_period_constraint(self.sdclk.cd_sd.clk, 1e9/sd_freq)
-
-        self.platform.add_false_path_constraints(
-            self.crg.cd_sys.clk,
-            self.sdclk.cd_sd.clk)
-
-        # led
+        # Led --------------------------------------------------------------------------------------
         led_counter = Signal(32)
         self.sync.sd += led_counter.eq(led_counter + 1)
         self.comb += platform.request("user_led", 0).eq(led_counter[26])
 
-        # analyzer
+        # Analyzer (optional) ----------------------------------------------------------------------
         if with_analyzer:
+            from litescope import LiteScopeAnalyzer
             analyzer_signals = [
                 self.sdphy.sdpads,
                 self.sdphy.cmdw.sink,
@@ -119,9 +117,11 @@ class SDSoC(SoCCore):
                 self.sdphy.datar.sink,
                 self.sdphy.datar.source
             ]
-            self.submodules.analyzer = LiteScopeAnalyzer(analyzer_signals, 2048, cd="sd",
+            self.submodules.analyzer = LiteScopeAnalyzer(analyzer_signals, 2048, clock_domain="sd",
                 csr_csv="../test/analyzer.csv")
             self.add_csr("analyzer")
+
+# Build --------------------------------------------------------------------------------------------
 
 def main():
     args = sys.argv[1:]
@@ -139,6 +139,7 @@ def main():
         vns = builder.build()
         soc.do_exit(vns)
     elif load:
+        from litex.build.openocd import OpenOCD
         print("[loading]...")
         prog = OpenOCD("board/minispartan6.cfg")
         prog.load_bitstream("build/gateware/top.bit")
--- examples/nexys4ddr.py
+++ examples/nexys4ddr.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# This file is Copyright (c) 2017-2018 Florent Kermarrec <florent@enjoy-digital.fr>
+# This file is Copyright (c) 2017-2020 Florent Kermarrec <florent@enjoy-digital.fr>
 # This file is Copyright (c) 2019 Kees Jongenburger <kees.jongenburger@gmail.com>
 # This file is Copyright (c) 2018 Rohit Kumar Singh <rohit91.2008@gmail.com>
 # License: BSD
@@ -10,8 +10,9 @@ import sys
 from migen import *
 from migen.genlib.resetsync import AsyncResetSynchronizer
 
+from litex.boards.platforms import nexys4ddr
+
 from litex.build.generic_platform import *
-from litex.build.xilinx import VivadoProgrammer
 
 from litex.soc.cores.clock import *
 from litex.soc.interconnect import stream
@@ -21,19 +22,13 @@ from litex.soc.cores.timer import Timer
 from litex.soc.interconnect.csr import *
 from litex.soc.integration.soc_core import *
 from litex.soc.integration.builder import *
-from litex.soc.interconnect import wishbone
 
 from litesdcard.phy import SDPHY
 from litesdcard.clocker import SDClockerS7
 from litesdcard.core import SDCore
 from litesdcard.bist import BISTBlockGenerator, BISTBlockChecker
 
-from litesdcard.emulator import SDEmulator, _sdemulator_pads
-
-from litex.boards.platforms import nexys4ddr
-
-from litescope import LiteScopeAnalyzer
-
+# CRG ----------------------------------------------------------------------------------------------
 
 class _CRG(Module):
     def __init__(self, platform, sys_clk_freq):
@@ -41,48 +36,57 @@ class _CRG(Module):
 
         # # #
 
+        clk100 = platform.request("clk100")
+        platform.add_period_constraint(clk100, 1e9/100e6)
+
         self.submodules.pll = pll = S7PLL(speedgrade=-1)
         self.comb += pll.reset.eq(~platform.request("cpu_reset"))
-        pll.register_clkin(platform.request("clk100"), 100e6)
-        pll.create_clkout(self.cd_sys,       sys_clk_freq)
+        pll.register_clkin(clk100, 100e6)
+        pll.create_clkout(self.cd_sys, sys_clk_freq)
 
+# SDCardSoC ----------------------------------------------------------------------------------------
 
-class SDSoC(SoCCore):
+class SDCardSoC(SoCCore):
     def __init__(self, with_cpu, with_emulator, with_analyzer):
         platform = nexys4ddr.Platform()
-        clk_freq = int(100e6)
-        sd_freq = int(100e6)
+        sys_clk_freq = int(100e6)
+        sd_clk_freq  = int(100e6)
+
+        # SoCCore ----------------------------------------------------------------------------------
         SoCCore.__init__(self, platform,
-                         clk_freq=clk_freq,
-                         cpu_type="lm32" if with_cpu else None,
-                         csr_data_width=32,
-                         with_uart=with_cpu,
-                         with_timer=with_cpu,
-                         ident="SDCard Test SoC",
-                         ident_version=True,
-                         integrated_rom_size=0x8000 if with_cpu else 0,
-                         integrated_main_ram_size=0x8000 if with_cpu else 0)
+            clk_freq                 = sys_clk_freq,
+            cpu_type                 = "lm32" if with_cpu else None,
+            csr_data_width           = 32,
+            with_uart                = with_cpu,
+            with_timer               = with_cpu,
+            ident                    = "SDCard Test SoC",
+            ident_version            = True,
+            integrated_rom_size      = 0x8000 if with_cpu else 0,
+            integrated_main_ram_size = 0x8000 if with_cpu else 0
+        )
 
-        self.submodules.crg = _CRG(platform, clk_freq)
+        # CRG --------------------------------------------------------------------------------------
+        self.submodules.crg = _CRG(platform, sys_clk_freq)
 
-        # bridge
+        # Serial bridge (optional) -----------------------------------------------------------------
         if not with_cpu:
-            self.submodules.bridge = UARTWishboneBridge(platform.request("serial"), clk_freq, baudrate=115200)
+            self.submodules.bridge = UARTWishboneBridge(platform.request("serial"), sys_clk_freq)
             self.add_wb_master(self.bridge.wishbone)
 
-        # emulator
+        # SDCard Emulator (optional) ---------------------------------------------------------------
         if with_emulator:
+            from litesdcard.emulator import SDEmulator, _sdemulator_pads
             sdcard_pads = _sdemulator_pads()
             self.submodules.sdemulator = SDEmulator(platform, sdcard_pads)
             self.add_csr("sdemulator")
         else:
             sdcard_pads = platform.request('sdcard')
 
-        # sd
+        # SDCard -----------------------------------------------------------------------------------
         self.comb += sdcard_pads.rst.eq(0)
-        self.submodules.sdclk = SDClockerS7()
-        self.submodules.sdphy = SDPHY(sdcard_pads, platform.device)
-        self.submodules.sdcore = SDCore(self.sdphy)
+        self.submodules.sdclk   = SDClockerS7()
+        self.submodules.sdphy   = SDPHY(sdcard_pads, platform.device)
+        self.submodules.sdcore  = SDCore(self.sdphy)
         self.submodules.sdtimer = Timer()
         self.add_csr("sdclk")
         self.add_csr("sdphy")
@@ -93,28 +97,25 @@ class SDSoC(SoCCore):
         self.submodules.bist_checker = BISTBlockChecker(random=True)
         self.add_csr("bist_generator")
         self.add_csr("bist_checker")
-
         self.comb += [
             self.sdcore.source.connect(self.bist_checker.sink),
             self.bist_generator.source.connect(self.sdcore.sink)
         ]
-
-        self.platform.add_period_constraint(self.crg.cd_sys.clk, 1e9/clk_freq)
-        self.platform.add_period_constraint(self.sdclk.cd_sd.clk, 1e9/sd_freq)
-        self.platform.add_period_constraint(self.sdclk.cd_sd_fb.clk, 1e9/sd_freq)
-
+        self.platform.add_period_constraint(self.sdclk.cd_sd.clk, 1e9/sd_clk_freq)
+        self.platform.add_period_constraint(self.sdclk.cd_sd_fb.clk, 1e9/sd_clk_freq)
         self.platform.add_false_path_constraints(
             self.crg.cd_sys.clk,
             self.sdclk.cd_sd.clk,
             self.sdclk.cd_sd_fb.clk)
 
-        # led
+        # Led --------------------------------------------------------------------------------------
         led_counter = Signal(32)
         self.sync.sd += led_counter.eq(led_counter + 1)
         self.comb += platform.request("user_led", 0).eq(led_counter[26])
 
-        # analyzer
+        # Analyzer (optional) ----------------------------------------------------------------------
         if with_analyzer:
+            from litescope import LiteScopeAnalyzer
             analyzer_signals = [
                 self.sdphy.sdpads,
                 self.sdphy.cmdw.sink,
@@ -124,9 +125,11 @@ class SDSoC(SoCCore):
                 self.sdphy.datar.sink,
                 self.sdphy.datar.source
             ]
-            self.submodules.analyzer = LiteScopeAnalyzer(analyzer_signals, 2048, cd="sd",
+            self.submodules.analyzer = LiteScopeAnalyzer(analyzer_signals, 2048, clock_domain="sd",
                 csr_csv="../test/analyzer.csv")
             self.add_csr("analyzer")
+
+# Build --------------------------------------------------------------------------------------------
 
 def main():
     args = sys.argv[1:]
@@ -139,10 +142,11 @@ def main():
         with_analyzer = "analyzer" in args
         print("[building]... cpu: {}, emulator: {}, analyzer: {}".format(
             with_cpu, with_emulator, with_analyzer))
-        soc = SDSoC(with_cpu, with_emulator, with_analyzer)
+        soc = SDCardSoC(with_cpu, with_emulator, with_analyzer)
         builder = Builder(soc, output_dir="build", csr_csv="../test/csr.csv")
         vns = builder.build()
     elif load:
+        from litex.build.xilinx import VivadoProgrammer
         print("[loading]...")
         prog = VivadoProgrammer()
         prog.load_bitstream("build/gateware/top.bit")
