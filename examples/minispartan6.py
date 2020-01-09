1593240265a267953387caa8b18646a47dbce750
Florent Kermarrec <florent@enjoy-digital.fr>
2020-01-09 11:18:34 +0100

    examples: keep now automatically added by litex.buid on constrained signals

--- examples/minispartan6.py
+++ examples/minispartan6.py
@@ -135,8 +135,6 @@ class SDSoC(SoCCore):
         self.platform.add_period_constraint(self.crg.cd_sys.clk, 1e9/clk_freq)
         self.platform.add_period_constraint(self.sdclk.cd_sd.clk, 1e9/sd_freq)
 
-        self.crg.cd_sys.clk.attr.add("keep")
-        self.sdclk.cd_sd.clk.attr.add("keep")
         self.platform.add_false_path_constraints(
             self.crg.cd_sys.clk,
             self.sdclk.cd_sd.clk)
--- examples/nexys4ddr.py
+++ examples/nexys4ddr.py
@@ -125,9 +125,6 @@ class SDSoC(SoCCore):
         self.platform.add_period_constraint(self.sdclk.cd_sd.clk, 1e9/sd_freq)
         self.platform.add_period_constraint(self.sdclk.cd_sd_fb.clk, 1e9/sd_freq)
 
-        self.crg.cd_sys.clk.attr.add("keep")
-        self.sdclk.cd_sd.clk.attr.add("keep")
-        self.sdclk.cd_sd_fb.clk.attr.add("keep")
         self.platform.add_false_path_constraints(
             self.crg.cd_sys.clk,
             self.sdclk.cd_sd.clk,
