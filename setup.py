c88db453321ccc58caa5fe55d00e7cd982bd125f
Florent Kermarrec <florent@enjoy-digital.fr>
2020-04-07 11:59:00 +0200

    setup.py: simplify, switch to Python3.6+ (using python_requires), remove version.
    - Deprecate Python 3.5, switch to Python 3.6+.
    - Remove which was not used or updated. We'll see to get this back when working on releases.

--- setup.py
+++ setup.py
@@ -1,36 +1,19 @@
 #!/usr/bin/env python3
 
-import sys
 from setuptools import setup
 from setuptools import find_packages
 
 
-if sys.version_info[:3] < (3, 3):
-    raise SystemExit("You need Python 3.3+")
-
-
 setup(
     name="litesdcard",
-	version="0.2.dev",
-	description="small footprint and configurable SD Card core",
-	long_description=open("README").read(),
-	author="Pierre-Olivier Vauboin",
-	author_email="po@lambdaconcept.com",
+    python_requires="~=3.6",
+	description="Small footprint and configurable SD Card core",
+	author="Pierre-Olivier Vauboin, Florent Kermarrec",
+	author_email="po@lambdaconcept.com, florent@enjoy-digital.fr",
 	url="http://enjoy-digital.fr",
-	download_url="https://github.com/lambdaconcept/litesdcard",
+	download_url="https://github.com/enjoy-digital/litesdcard",
 	test_suite="test",
     license="BSD",
-    platforms=["Any"],
-    keywords="HDL ASIC FPGA hardware design",
-	classifiers=[
-		"Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)",
-		"Environment :: Console",
-		"Development Status :: Alpha",
-		"Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-		"Operating System :: OS Independent",
-		"Programming Language :: Python",
-    ],
     packages=find_packages(exclude=("test*", "sim*", "doc*", "examples*")),
     include_package_data=True,
 )
