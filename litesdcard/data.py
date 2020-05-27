2d086a187feccceeb1cb4f3704b6fd74496510b8
Florent Kermarrec <florent@enjoy-digital.fr>
2020-05-28 00:05:24 +0200

    litesdcard/data: review/simplify.
    Since SDDataWriter has async_read property set to True on port in the integration,
    add an assert and use this to simplify/speed-up the core.

--- litesdcard/data.py
+++ litesdcard/data.py
@@ -1,63 +1,53 @@
-# This file is Copyright (c) 2017-2018 Florent Kermarrec <florent@enjoy-digital.fr>
+# This file is Copyright (c) 2017-2020 Florent Kermarrec <florent@enjoy-digital.fr>
 # This file is Copyright (c) 2020 Antmicro <www.antmicro.com>
 # License: BSD
-# Based on litesdcard/bist.py
-
-
-from functools import reduce
-from operator import xor
 
 from migen import *
+
+from litex.gen.common import reverse_bytes
 
 from litex.soc.interconnect import stream
 from litex.soc.interconnect.csr import *
 
-from litex.gen import *
+# Helpers ------------------------------------------------------------------------------------------
 
-def with_endianness(endianness, src):
-    if endianness == "big":
-        return src
-    else:
-        return reverse_bytes(src)
+def format_bytes(s, endianness):
+    return {"big": s, "little": reverse_bytes(s)}[endianness]
 
+# SDDataWriter (Read a 512-bytes Block from Mem and stream it on Source) ---------------------------
 
 @ResetInserter()
 class _SDDataWriter(Module):
     def __init__(self, port, endianness):
+        assert port.async_read == True
         self.source = source = stream.Endpoint([("data", 32)])
-        self.start = Signal()
-        self.done = Signal()
+        self.start  = Signal()
+        self.done   = Signal()
 
-        self.endianness = endianness
-        self.port = port
+        # # #
 
-        datcnt = Signal(7)
+        count = Signal(max=512//4)
 
-        fsm = FSM(reset_state="IDLE")
-        self.submodules += fsm
+        self.submodules.fsm = fsm = FSM(reset_state="IDLE")
         fsm.act("IDLE",
             If(self.start,
-                NextValue(datcnt, 0),
+                NextValue(count, 0),
                 NextState("RUN")
             )
         )
+        self.comb += port.adr.eq(count)
+        self.comb += source.data.eq(format_bytes(port.dat_r, endianness))
         fsm.act("RUN",
-            self.port.adr.eq(datcnt),
-            source.data.eq(with_endianness(self.endianness, self.port.dat_r)),
+            port.adr.eq(count),
             source.valid.eq(1),
-            source.last.eq(datcnt == (512//4 - 1)),
+            source.last.eq(count == (512//4 - 1)),
             If(source.ready,
+                NextValue(count, count + 1),
                 If(source.last,
                     NextState("DONE")
-                ).Else(
-                    NextValue(datcnt, datcnt + 1),
-                    NextState("RUN_INC"), # One cycle for memory access
                 )
             )
         )
-        fsm.act("RUN_INC",
-            self.port.adr.eq(datcnt),
-            NextState("RUN"))
         fsm.act("DONE",
             self.done.eq(1)
         )
@@ -66,9 +56,11 @@ class _SDDataWriter(Module):
 class SDDataWriter(Module, AutoCSR):
     def __init__(self, port, endianness):
         self.source = source = stream.Endpoint([("data", 32)])
-        self.reset = CSR()
-        self.start = CSR()
-        self.done = CSRStatus()
+        self.reset  = CSR()
+        self.start  = CSR()
+        self.done   = CSRStatus()
+
+        # # #
 
         core = _SDDataWriter(port, endianness)
         self.submodules += core
@@ -80,39 +72,36 @@ class SDDataWriter(Module, AutoCSR):
             self.done.status.eq(core.done),
         ]
 
+# SDDataReader (Receive a 512-bytes Block on Sink and write it to Mem) -----------------------------
 
 @ResetInserter()
 class _SDDataReader(Module):
     def __init__(self, port, endianness):
-        self.sink = sink = stream.Endpoint([("data", 32)])
-        self.start = Signal()
-        self.done = Signal()
-        self.errors = Signal(32)
+        self.sink   = sink = stream.Endpoint([("data", 32)])
+        self.start  = Signal()
+        self.done   = Signal()
 
-        self.endianness = endianness
-        self.port = port
+        # # #
 
-        datcnt = Signal(7)
+        count = Signal(max=512//4)
 
-        fsm = FSM(reset_state="IDLE")
-        self.submodules += fsm
+        self.submodules.fsm = fsm = FSM(reset_state="IDLE")
         fsm.act("IDLE",
             sink.ready.eq(1),
             self.done.eq(1),
             If(self.start,
-                NextValue(datcnt, 0),
-                NextValue(self.errors, 0),
+                NextValue(count, 0),
                 NextState("RUN")
             )
         )
+        self.comb += port.adr.eq(count),
+        self.comb += port.dat_w.eq(format_bytes(sink.data, endianness))
         fsm.act("RUN",
             sink.ready.eq(1),
-            self.port.adr.eq(datcnt),
-            self.port.dat_w.eq(with_endianness(self.endianness, sink.data)),
             If(sink.valid,
-                self.port.we.eq(1),
-                NextValue(datcnt, datcnt + 1),
-                If(sink.last | (datcnt == (512//4 - 1)),
+                port.we.eq(1),
+                NextValue(count, count + 1),
+                If(sink.last | (count == (512//4 - 1)),
                     NextState("DONE")
                 )
             )
@@ -124,11 +113,12 @@ class _SDDataReader(Module):
 
 class SDDataReader(Module, AutoCSR):
     def __init__(self, port, endianness):
-        self.sink = sink = stream.Endpoint([("data", 32)])
-        self.reset = CSR()
-        self.start = CSR()
-        self.done = CSRStatus()
-        self.errors = CSRStatus(32)
+        self.sink   = sink = stream.Endpoint([("data", 32)])
+        self.reset  = CSR()
+        self.start  = CSR()
+        self.done   = CSRStatus()
+
+        # # #
 
         core = _SDDataReader(port, endianness)
         self.submodules += core
@@ -138,5 +128,4 @@ class SDDataReader(Module, AutoCSR):
             core.reset.eq(self.reset.re),
             core.start.eq(self.start.re),
             self.done.status.eq(core.done),
-            self.errors.status.eq(core.errors)
         ]
