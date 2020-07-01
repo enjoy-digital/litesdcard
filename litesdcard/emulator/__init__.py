d9d26096cc359a414a8b2409cc624fd5a59c6dd7
Florent Kermarrec <florent@enjoy-digital.fr>
2020-07-01 09:59:56 +0200

    emulator: update copyrights.

--- litesdcard/emulator/__init__.py
+++ litesdcard/emulator/__init__.py
@@ -1,4 +1,6 @@
-# Copyright (c) 2017 Micah Elizabeth Scott
+# This file is Copyright (c) 2017 Micah Elizabeth Scott
+# This file is Copyright (c) 2020 Florent Kermarrec <florent@enjoy-digital.fr>
+# License: BSD
 
 from litesdcard.emulator.core import SDEmulator
 from litesdcard.emulator.linklayer import SDLinkLayer, _sdemulator_pads
--- litesdcard/emulator/core.py
+++ litesdcard/emulator/core.py
@@ -1,4 +1,6 @@
-# Copyright (c) 2017 Micah Elizabeth Scott
+# This file is Copyright (c) 2017 Micah Elizabeth Scott
+# This file is Copyright (c) 2020 Florent Kermarrec <florent@enjoy-digital.fr>
+# License: BSD
 
 from migen import *
 
