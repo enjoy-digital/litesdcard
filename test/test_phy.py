3cc41a9282144bb8c6e4a2caa554e90504647d38
Florent Kermarrec <florent@enjoy-digital.fr>
2020-07-08 08:05:07 +0200

    test: add empty test_phy skeleton.

---
+++ test/test_phy.py
@@ -0,0 +1,34 @@
+# This file is Copyright (c) 2020 Florent Kermarrec <florent@enjoy-digital.fr>
+# License: BSD
+
+import unittest
+
+from migen import *
+
+class TestPHY(unittest.TestCase):
+    def test_clocker(self):
+        pass
+
+    def test_phyr_1b(self):
+        pass
+
+    def test_phyr_4b(self):
+        pass
+
+    def test_phyinit(self):
+        pass
+
+    def test_phycmdw(self):
+        pass
+
+    def test_phycmdr(self):
+        pass
+
+    def test_phycrc(self):
+        pass
+
+    def test_phydataw(self):
+        pass
+
+    def test_phydatar(self):
+        pass
