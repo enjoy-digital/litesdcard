09147ae74bf2b6f6c50288052daaea3a740cd712
Florent Kermarrec <florent@enjoy-digital.fr>
2020-08-26 09:09:11 +0200

    test/test_crc: add bytes2dats/dats2bytes to simplify and add test with SDCard tuning block.

--- test/test_crc.py
+++ test/test_crc.py
@@ -10,11 +10,41 @@ from migen import *
 
 from litesdcard.crc import *
 
+def bytes2dats(bytes):
+    d = ["" for i in range(4)]
+    # bytes -> dat bits
+    for b in _bytes:
+        for i in range(4):
+            d[i] += str((b >> (4*1 + i)) & 0x1)
+            d[i] += str((b >> (4*0 + i)) & 0x1)
+    # dat bits -> dat value
+    for i in range(4):
+        d[i] = int(d[i], 2)
+    return d
+
+def dats2bytes(dats):
+    assert len(dats) == 4
+    # dat value -> dat bits
+    b = [list("{:b}".format(dats[i])) for i in range(4)]
+    if len(b[0]) % 2 != 0:
+        for i in range(4):
+            b[i].insert(0, 0)
+    # dat bits -> bytes
+    _b = []
+    while len(b[0]):
+        v = 0
+        for i in range(2):
+            v |= (int(b[0].pop(0)) << 4*(1-i) + 0)
+            v |= (int(b[1].pop(0)) << 4*(1-i) + 1)
+            v |= (int(b[2].pop(0)) << 4*(1-i) + 2)
+            v |= (int(b[3].pop(0)) << 4*(1-i) + 3)
+        _b.append(v)
+    return _b
+
 class TestCRC(unittest.TestCase):
-    def test_crc_inserter(self):
+    def crc_inserter_test(self, data, crc):
         def stim_gen(dut):
             yield
-            data = [0xff]*512*4
             yield dut.sink.valid.eq(1)
             for i in range(len(data)):
                 if (i == len(data) - 1):
@@ -26,15 +56,25 @@ class TestCRC(unittest.TestCase):
             yield
 
         def check_gen(dut):
-            data = [0xff]*512*4 + [0x0f, 0xff, 0xff, 0xff, 0xf0, 0xf0, 0x00, 0x0f] # 0x7fa1
+            data_crc = data + dats2bytes(crc)
             yield dut.source.ready.eq(1)
-            for i in range(len(data)):
+            for i in range(len(data_crc)):
                 while (yield dut.source.valid) == 0:
                     yield
-                print("{:02x} vs {:02x}".format(data[i], (yield dut.source.data)))
-                #self.assertEqual(data[i], (yield dut.source.data))
-                self.assertEqual((yield dut.source.last), int(i == len(data) - 1))
+                #print("{:02x} vs {:02x}".format(data_crc[i], (yield dut.source.data)))
+                self.assertEqual(data_crc[i], (yield dut.source.data))
+                self.assertEqual((yield dut.source.last), int(i == len(data_crc) - 1))
                 yield
 
         dut = CRC16Inserter()
         run_simulation(dut, [stim_gen(dut), check_gen(dut)], vcd_name="sim.vcd")
+
+    def test_crc_inserter_ones(self):
+        self.crc_inserter_test(data=[0xff]*512*4, crc=[0x7fa1, 0x7fa1, 0x7fa1, 0x7fa1])
+
+    def test_crc_inserter_tuning_block(self):
+        from litesdcard.common import SDCARD_TUNING_BLOCK
+        data = []
+        for word in SDCARD_TUNING_BLOCK:
+            data += word.to_bytes(4, "big")
+        self.crc_inserter_test(data=data, crc=[0xe946, 0x8d06, 0xa2e5, 0xc59f])
