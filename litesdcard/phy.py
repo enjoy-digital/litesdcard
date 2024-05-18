enjoy-digital/litesdcard

commit 15bc2db36607c2cebcf21f37b8c3e496a0c8fb68

phy: Avoid Xilinx's specific BUFG and use synchronous logic to improve timings.

litesdcard/phy.py
@@ -60,22 +60,15 @@ class SDPHYClocker(LiteXModule):
         cases["default"] = clk.eq(clks[0])
         for i in range(2, 9):
             cases[2**i] = clk.eq(clks[i-1])
-        self.comb += Case(self.divider.storage, cases)
-
-        # FIXME: Use BUFG to improve timings on large Xilinx desings, try to improve and make it more generic.
-        from litex.gen import LiteXContext
-        from litex.build.xilinx import XilinxPlatform
-        if isinstance(LiteXContext.platform, XilinxPlatform):
-            self.specials += Instance("BUFG", i_I=clk & ~clk_d, o_O=self.ce)
-        else:
-            self.comb += self.ce.eq(clk & ~clk_d)
+        self.sync += Case(self.divider.storage, cases)
+        self.sync += self.ce.eq(clk & ~clk_d)
 
         # Ensure we don't get short pulses on the SDCard Clk.
         ce_delayed = Signal()
         ce_latched = Signal()
         self.sync += If(clk_d, ce_delayed.eq(self.clk_en))
         self.comb += If(clk_d, ce_latched.eq(self.clk_en)).Else(ce_latched.eq(ce_delayed))
-        self.comb += self.clk.eq(~clk & ce_latched)
+        self.sync += self.clk.eq(~clk & ce_latched)
 
 # SDCard PHY Read ----------------------------------------------------------------------------------
 

commit c050ce91ebb7d34fa0442a2e4555981b06728881

phy: restore operation at lower (50MHz) sys-clk-freq
Following commit 15bc2db, LiteSDCard no longer works properly at
the low end of system clock speeds (e.g., 50MHz). Reverting the
way `self.ce` and `self.clk` are generated to `comb` instead of
`sync`, allows LiteSDCard to operate at the full previous range
of system clock speeds.

Fixes: 15bc2db
Signed-off-by: Gabriel Somlo <gsomlo@gmail.com>

litesdcard/phy.py
@@ -62,14 +62,14 @@ class SDPHYClocker(LiteXModule):
         # SDCard CE Generation.
         clk_d = Signal()
         self.sync += clk_d.eq(clk)
-        self.sync += self.ce.eq(clk & ~clk_d)
+        self.comb += self.ce.eq(clk & ~clk_d)
 
         # Ensure we don't get short pulses on the SDCard Clk.
         ce_delayed = Signal()
         ce_latched = Signal()
         self.sync += If(clk_d, ce_delayed.eq(self.clk_en))
         self.comb += If(clk_d, ce_latched.eq(self.clk_en)).Else(ce_latched.eq(ce_delayed))
-        self.sync += self.clk.eq(~clk & ce_latched)
+        self.comb += self.clk.eq(~clk & ce_latched)
 
 # SDCard PHY Read ----------------------------------------------------------------------------------
 